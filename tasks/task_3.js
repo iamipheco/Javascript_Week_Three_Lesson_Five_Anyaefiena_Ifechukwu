ef74fac6648c22853aa7ee36009c54d3c3e060db
Ipheco <iamipheco@gmail.com>
2023-07-17 23:57:41 +0100

    modify lesson five of week three

---
+++ tasks/task_3.js
@@ -0,0 +1,12 @@
+// Modify elements in an array
+// Create an array with multiple elements.
+// Use array indexing to access a specific element.
+// Modify the element value using assignment.
+// Log the array to the console to see the updated value.
+
+const nameOfGirls = ['Chizoba', 'Tochi', 'Clementina', 'Chidimma', 'Deborah'] // Created an array with multiple elements.
+const nameOfSecondGirl = nameOfGirls[1] // Used array indexing to access a specific element which is the name of the second girl in the array
+
+nameOfGirls[1] = 'Chioma' // Modified the element value using assignment to assign a new value at index of 1 which is the second element
+console.log(nameOfGirls) // Output the New value of nameOfGirls : [ 'Chizoba', 'Chioma', 'Clementina', 'Chidimma', 'Deborah' ]
+console.log(nameOfSecondGirl)
---
+++ tasks/task_4.js
@@ -0,0 +1,11 @@
+// Iterate over an array
+// Create an array with multiple elements.
+// Use a loop (such as for or forEach()) to iterate over the array.
+// Perform an action on each element, such as logging it to the console.
+
+const namesOfBoys = ['Ipheco', 'Prosper', 'Martins', 'Winners', 'Arinze'] // Created an array with multiple elements.
+
+// Use a loop (such as for or forEach()) to iterate over the array.
+namesOfBoys.forEach((listBoysName) => {
+  console.log(listBoysName) // Perform an action on each element, such as logging it to the console.
+})
